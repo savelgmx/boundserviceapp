7033166758e5dadb0b239a5b044f81470e6229b9
Andrey Savitsky <savel@gmx.com>
2018-06-25 12:10:25 +0700

    first commit before any changes

---
+++ app/src/main/java/fb/fandroid/adv/boundserviceapp/MainActivity.java
@@ -0,0 +1,124 @@
+package fb.fandroid.adv.boundserviceapp;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+/*
+    При запуске приложения создать Bound Service
+ в потоке которого постепенно будет меняться значение прогресса и,
+    соответственно, обновляться ProgressBar. Если брать максимум ProgressBar - 100%,
+    то значение прогресса должно меняться на 5% каждые 200 миллисекунд.
+    По достижению 100% ProgressBar перестает заполняться.
+    В любой момент по нажатию на кнопку шкала уменьшается на 50%, но не меньше 0%.
+    (75% -> 25%; 35% -> 0%)
+Дополнительно: Каждый раз по достижении 100% появляется тост о завершении загрузки.
+ */
+
+public class MainActivity extends AppCompatActivity {
+    BoundService mBoundService;
+    boolean mServiceBound = false;
+    private int progressStatus=0;
+    private Handler handler=new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final TextView timestampText = (TextView) findViewById(R.id.timestamp_text);
+        Button printTimestampButton = (Button) findViewById(R.id.print_timestamp);
+        Button stopServiceButon = (Button) findViewById(R.id.stop_service);
+        final ProgressBar progressBar =(ProgressBar)findViewById(R.id.progressBar);
+        //----progress bar here------------------------
+        // Start long running operation in a background thread
+        new Thread(new Runnable() {
+            public void run() {
+                while (progressStatus < 100) {
+                    progressStatus += 1;
+                    // Update the progress bar and display the
+                    //current value in the text view
+                    handler.post(new Runnable() {
+                        public void run() {
+                            progressBar.setProgress(progressStatus);
+                            timestampText.setText(progressStatus+"/"+progressBar.getMax());
+                        }
+                    });
+                    try {
+                        // Sleep for 200 milliseconds.
+                        Thread.sleep(200);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+
+        //----end progress bar starting---------------------
+
+
+
+        printTimestampButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mServiceBound) {
+                    timestampText.setText(mBoundService.getTimestamp());
+                }
+            }
+        });
+
+        stopServiceButon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mServiceBound) {
+                    unbindService(mServiceConnection);
+                    mServiceBound = false;
+                }
+                Intent intent = new Intent(MainActivity.this,
+                        BoundService.class);
+                stopService(intent);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent = new Intent(this, BoundService.class);
+        startService(intent);
+        bindService(intent, mServiceConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mServiceBound) {
+            unbindService(mServiceConnection);
+            mServiceBound = false;
+        }
+    }
+
+    private ServiceConnection mServiceConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mServiceBound = false;
+        }
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            BoundService.MyBinder myBinder = (BoundService.MyBinder) service;
+            mBoundService = myBinder.getService();
+            mServiceBound = true;
+        }
+    };
+}
